f268772cdcf0fa40f5091f9642349ee2783c6e09
= <=>
2025-04-17 15:08:43 -0700

    stylesheet

--- script.js
+++
@@ -1,5 +0,0 @@
-const symbols = ['🍎','🍌','🍒','🍇','🍉','🍋','🍓','🍑'];
-let cards = [...symbols, ...symbols]
-const board = document.querySelector('.game-board');
-
-let firstCard = null, secondCard = null, lockBoard = false;
